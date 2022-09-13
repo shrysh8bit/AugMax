2e82aa8aae2879579c9e7c7ad6f323c004548a0f
SoumiDas <33958408+SoumiDas@users.noreply.github.com>
2022-09-13 18:19:52 +0530

    Add files via upload

---
+++ helpercodes/augmax_cifar10/test_fgsm.py
@@ -0,0 +1,201 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from PIL import Image
+from subsetpack.fgsm import FGSM
+from torch.utils.data import Dataset
+import torch.backends.cudnn as cudnn
+from skimage.util import random_noise
+import torchvision
+import cv2
+import torchvision.transforms as transforms
+import numpy as np
+import random
+import os
+import argparse
+
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', action='store_true',
+                    help='resume from checkpoint')
+args = parser.parse_args()
+
+############ Model Architecture ##########
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion *
+                               planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+def _resnet(block, layers):
+    model = ResNet(block, layers)
+    return model
+
+def ResNet18():
+    return _resnet(BasicBlock, [2, 2, 2, 2])
+
+
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+# Data
+print('==> Preparing data..')
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+
+testset = torchvision.datasets.CIFAR10(
+    root='./data/', train=False, download=True, transform=transform_test)
+
+net = ResNet18()
+net = net.to(device)
+net.load_state_dict(torch.load('./checkpoint_sub/ckpt.pth')['net'])
+
+net1 = ResNet18()
+net1 = net1.to(device)
+net1.load_state_dict(torch.load('./checkpoint_sub/ckpt_sub20_alpha_otheraug_all.pth')['net'])
+
+
+testloaderb = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+print(len(testloaderb))
+
+
+# Model
+torch.manual_seed(123)
+print('==> Building model..')
+criterion = nn.CrossEntropyLoss()
+    
+def test(epoch,net):
+    acc = []
+    epv = []
+    global best_acc
+    net.eval()    
+    #with torch.no_grad():
+    for ep in [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]:
+        test_loss = 0
+        correct = 0
+        total = 0
+        atk = FGSM(net, eps=ep)
+        for batch_idx, (inputs, targets) in enumerate(testloaderb):
+            inputs, targets = inputs.to(device), targets.to(device)
+            _,inputs = atk(inputs, targets)
+            outputs = net1(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+        # Save checkpoint.
+        acc.append(100.*correct/total)
+        epv.append(ep)
+    print(acc)
+    print(np.mean(acc))
+    print(epv)
+
+for epoch in range(1): #Run till convergence
+    print("Testing")
+    #test1(epoch)
+    test(epoch,net)
+    #scheduler.step()
